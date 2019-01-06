0a2513f08c2ac349cb4194aabfa757f261bef875
Milo WIssig <mwissig@gmail.com>
2019-01-06 16:58:06 -0500

    registration works but login does not

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 
   get 'users' => 'users#index'
   get 'users/edit'
@@ -7,4 +10,9 @@ Rails.application.routes.draw do
   get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
+  resources :users do
+ delete 'delete' => 'users#destroy'
+end
+
 end
