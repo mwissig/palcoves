1af0501ab8e9df0adb1f3453fb5a272aa85c19d9
Milo WIssig <mwissig@gmail.com>
2019-01-10 18:16:41 -0500

    somee layout issues fixed"

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :username
   belongs_to :post
   # has_one_attached :image
-  validates :body, presence: true, length: { minimum: 20 }
+  validates :body, presence: true, length: { minimum: 12 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   has_one_attached :image
   has_many :comments
   validates :title, presence: true, length: { maximum: 256, minimum: 3 }
-  validates :body, presence: true, length: { minimum: 20 }
+  validates :body, presence: true, length: { minimum: 12 }
 end
