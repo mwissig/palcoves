4b493ace7512fe0edeaf05bc3afb4713f956d792
Milo WIssig <mwissig@gmail.com>
2019-01-11 00:02:56 -0500

    sharing of posts

--- app/models/username.rb
+++ app/models/username.rb
@@ -1,9 +1,9 @@
 class Username < ApplicationRecord
   belongs_to :user
-    has_many :posts
+  has_many :posts
   has_one_attached :avatar
   before_save { self.name = name.downcase }
-before_save { self.slug = name }
+  before_save { self.slug = name }
 # before_save { self.user_id = @current_user.id }
   validates :name, presence: true, length: { maximum: 100, minimum: 3 }, format: {  with: /\A[a-zA-Z0-9]+\Z/, message: "username contains invalid characters" }
   extend FriendlyId
