3237c30e5f1c4d6f507556a6df472dcab4390ba6
Milo WIssig <mwissig@gmail.com>
2019-01-21 21:21:00 -0500

    mailer test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,5 @@ config.action_mailer.smtp_settings = {
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
+config.action_mailer.default_url_options = { :host => 'palcoves.herokuapp.com' }
 end
