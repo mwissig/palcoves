f5d9ef2dcab039bd65957f57b5596937e3c2b419
Milo WIssig <mwissig@gmail.com>
2019-01-23 11:10:19 -0500

    test distinct

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,35 +2,29 @@ class PagesController < ApplicationController
   def home
     @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
     if logged_in?
-    @tags = []
-    @tagged_posts = []
-    @current_user.usernames.each do |us|
-      Follow.all.where(username_id: us.id).each do |fol|
-        @tags.push(fol)
-      end
-    end
-    @tags.uniq!
-    @tags.each do |tag|
-      if tag.recipient_id == nil
-        post_by_tags = Post.pluck(:tags)
-
-        matches = []
-
-      post_by_tags.each do |t|
-        if t != nil
-          matches << t if t.downcase.include?(tag.tag)
+      @tags = []
+      @tagged_posts = []
+      @current_user.usernames.each do |us|
+        Follow.all.where(username_id: us.id).each do |fol|
+          @tags.push(fol)
         end
       end
-
-      matches.each do |match|
-        x = Post.where(tags: match)
-        x.each do |y|
-          @tagged_posts << y
-        end
-      end
-
-
-
+      @tags.uniq!
+      @tags.each do |tag|
+        if tag.recipient_id == nil
+          post_by_tags = Post.pluck(:tags)
+          matches = []
+          post_by_tags.each do |t|
+            if t != nil
+              matches << t if t.downcase.include?(tag.tag)
+            end
+          end
+          matches.each do |match|
+            matched_posts = Post.where(tags: match)
+            matched_posts.each do |post|
+              @tagged_posts << post
+            end
+          end
       else
         @followed_accts = Post.all.where(username_id: tag.recipient_id)
         @followed_accts.each do |post|
@@ -39,12 +33,7 @@ class PagesController < ApplicationController
       end
 
     end
-    tagposts = @tagged_posts.uniq!
-      if tagposts != nil
-    @posts = Post.all.where(id: tagposts.map(&:id)).order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
-  else
-    @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
-  end
+        @posts = Post.all.where(id: @tagged_posts.map(&:id)).distinct.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   else
         @posts = Post.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
   end
