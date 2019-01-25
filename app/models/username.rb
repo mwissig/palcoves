52e450bd1764142526930f9dd9856fc8c5f25d2a
Milo WIssig <mwissig@gmail.com>
2019-01-25 09:42:12 -0500

    regex edit

--- app/models/username.rb
+++ app/models/username.rb
@@ -9,7 +9,7 @@ class Username < ApplicationRecord
   before_save { self.name = name.downcase }
   before_save { self.slug = name }
 # before_save { self.user_id = @current_user.id }
-  validates :name, presence: true, length: { maximum: 100, minimum: 3 }, format: {  with: /\A[a-zA-Z0-9]+\Z/, message: "username contains invalid characters" }
+  validates :name, presence: true, length: { maximum: 100, minimum: 3 }, format: {  with: /\A[-_, a-zA-Z0-9]+\Z/, message: "username contains invalid characters" }
   extend FriendlyId
   friendly_id :name, use: :slugged
 end
