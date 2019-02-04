02a60f1f875a50598182dd7f8ef36bc2a0f285b5
Milo WIssig <mwissig@gmail.com>
2019-02-04 16:57:29 -0500

    basic pm structure

--- app/controllers/pms_controller.rb
+++ app/controllers/pms_controller.rb
@@ -5,6 +5,7 @@ class PmsController < ApplicationController
   end
 
   def show
+      @pm = Pm.new
             @username = Username.friendly.find(params[:username_id])
             @sender = Username.friendly.find(params[:id])
             @msgs = []
@@ -15,6 +16,33 @@ class PmsController < ApplicationController
               @msgs << pm
             end
             @messages = Pm.all.where(id: @msgs.map(&:id))
-            @conversation = @messages.order("created_at DESC").paginate(:page => params[:page], :per_page => 50)
-  end
+            @conversation = @messages.order("created_at ASC").paginate(:page => params[:page], :per_page => 50)
+end
+
+def create
+  if logged_in?
+    @pm = Pm.new(pm_params)
+    @pm.save!
+    if @pm.save
+          Notification.create(
+            kind: 'private_message',
+            username_id: @pm.recipient_id,
+            sender_id: @pm.username_id,
+            body: @pm.body,
+            read: false
+          )
+    end
+    redirect_back(fallback_location: root_path)
+    else
+      render 'new'
+      msg = @pm.errors.full_messages
+      flash.now[:error] = msg
+    end
+end
+
+private
+
+def pm_params
+params.require(:pm).permit(:username_id, :body, :recipient_id)
+end
 end
