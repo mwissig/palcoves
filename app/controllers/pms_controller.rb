d200b0c8ae72c59665062020badd85c641e91517
Milo WIssig <mwissig@gmail.com>
2019-02-04 16:18:43 -0500

    working on om screen

--- app/controllers/pms_controller.rb
+++ app/controllers/pms_controller.rb
@@ -1,7 +1,20 @@
 class PmsController < ApplicationController
   def index
+        @username = Username.friendly.find(params[:username_id])
+    @pms_to_me = Pm.where(recipient_id: @username.id)
   end
 
   def show
+            @username = Username.friendly.find(params[:username_id])
+            @sender = Username.friendly.find(params[:id])
+            @msgs = []
+            Pm.where(username_id: @username.id).each do |pm|
+              @msgs << pm
+            end
+            Pm.where(recipient_id: @username.id).each do |pm|
+              @msgs << pm
+            end
+            @messages = Pm.all.where(id: @msgs.map(&:id))
+            @conversation = @messages.order("created_at DESC").paginate(:page => params[:page], :per_page => 50)
   end
 end
