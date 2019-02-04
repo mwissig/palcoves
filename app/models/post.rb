7254e707058d730e752cb223a869c08bda76193d
Milo WIssig <mwissig@gmail.com>
2019-02-04 14:22:36 -0500

    dependent destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ require 'bb-ruby'
 class Post < ApplicationRecord
   belongs_to :username
   has_one_attached :image
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { maximum: 256, minimum: 3 }
   validates :body, presence: true, length: { minimum: 12 }
 end
